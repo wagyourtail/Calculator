fe240b4e083d63514be910aaf187752af4f29644
wagyourtail <wagyourtail@wagyourtail.xyz>
2021-05-27 22:41:23 -0600

    initial impl

---
+++ src/main/java/xyz/wagyourtail/calculator/DefaultOperations.java
@@ -0,0 +1,65 @@
+package xyz.wagyourtail.calculator;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class DefaultOperations {
+
+    public static Map<String, Function<Double[], Double>> getFunctionOperations() {
+        Map<String, Function<Double[], Double>> ops = new LinkedHashMap<>();
+
+        return ops;
+    }
+
+    public static Map<String, Function<Double[], Double>> getSymbolOperations() {
+        Map<String, Function<Double[], Double>> ops = new LinkedHashMap<>();
+        ops.put("^", DefaultOperations::power);
+        ops.put("*", DefaultOperations::multiply);
+        ops.put("/", DefaultOperations::divide);
+        ops.put("+",  DefaultOperations::add);
+        ops.put("-", DefaultOperations::subtract);
+        return ops;
+    }
+
+    public static double power(Double ...args) {
+        double start = args[args.length - 1];
+        for (int i = args.length - 2; i > -1; --i) {
+            start = Math.pow(args[i], start);
+        }
+        return start;
+    }
+
+    public static double multiply(Double ...args) {
+        double start = args[0];
+        for (int i = 1; i < args.length; ++i) {
+            start = start * args[i];
+        }
+        return start;
+    }
+
+    public static double divide(Double ...args) {
+        double start = args[0];
+        for (int i = 1; i < args.length; ++i) {
+            start = start / args[i];
+        }
+        return start;
+    }
+    public static double add(Double ...args) {
+        double start = args[0];
+        for (int i = 1; i < args.length; ++i) {
+            start = start + args[i];
+        }
+        return start;
+    }
+
+    public static double subtract(Double ...args) {
+        double start = args[0];
+        for (int i = 1; i < args.length; ++i) {
+            start = start - args[i];
+        }
+        return start;
+    }
+}
---
+++ src/main/java/xyz/wagyourtail/calculator/ExtensibleCalculator.java
@@ -0,0 +1,79 @@
+package xyz.wagyourtail.calculator;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtensibleCalculator {
+    protected static final Map<String, Function<Double[], Double>> functionOperations = DefaultOperations.getFunctionOperations();
+    protected static final Map<String, Function<Double[], Double>> symbolOperations = DefaultOperations.getSymbolOperations();
+    private static final String doubleMatch = "(?:\\d*\\.?\\d+|NaN)";
+
+
+    public static void main(String... args) {
+        String input = String.join(" ", args);
+        System.out.println(calculate(input));
+    }
+
+
+    public static String calculate(String input) {
+        //prepare input
+        input = "(" + input + ")";
+
+        while (!input.matches(doubleMatch)) {
+            //remove excess parenthesis
+            input = replaceFunction(input, Pattern.compile("(\\(+)\\s*(" + doubleMatch +")\\s*(\\)+)"), it -> {
+                if (it[1].length() < it[3].length()) {
+                    return it[2] + it[3].substring(it[1].length());
+                } else if (it[3].length() < it[1].length()) {
+                    return it[1].substring(it[3].length()) + it[2];
+                }
+                return it[2];
+            });
+
+            //parse ready symbol groups
+            input = replaceFunction(input, Pattern.compile("\\(([^()]+)\\)"), it -> parseSymbols(it[1]));
+
+            //TODO: functions
+        }
+
+        return input;
+    }
+
+    /**
+     * only use on symbol only math
+     */
+    private static String parseSymbols(String input) {
+        while (!input.matches(doubleMatch)) {
+            for (Map.Entry<String, Function<Double[], Double>> sym : symbolOperations.entrySet()) {
+                input = replaceFunction(input, Pattern.compile("(" + doubleMatch + ")\\s*(?:\\" + sym.getKey() + "\\s*(" + doubleMatch + "))+"), it -> {
+                    Double[] args = Arrays.stream(it[0].split("\\" + sym.getKey())).map(Double::parseDouble).toArray(Double[]::new);
+                    return Double.toString(sym.getValue().apply(args));
+                });
+            }
+        }
+        return input;
+    }
+
+    public static @NotNull String replaceFunction(@NotNull String str, @NotNull Pattern pattern, @NotNull Function<String[], String> replaceFn) {
+        Matcher m = pattern.matcher(str);
+        int offset = 0;
+        while (m.find()) {
+            String[] args = new String[m.groupCount() + 1];
+            for (int i = 0; i < args.length; ++i) {
+                args[i] = m.group(i);
+            }
+            String replacement = replaceFn.apply(args);
+            int len = m.end() - m.start();
+            str = str.substring(0, m.start() + offset) + replacement + str.substring(m.end() + offset);
+            offset = replacement.length() - len;
+        }
+        return str;
+    }
+}
