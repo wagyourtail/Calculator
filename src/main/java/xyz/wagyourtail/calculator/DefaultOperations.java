df64e2401b73992d50f9b92ab54a342c933003ed
wagyourtail <wagyourtail@wagyourtail.xyz>
2021-05-28 00:37:51 -0600

    functions, constants and modulo

--- src/main/java/xyz/wagyourtail/calculator/DefaultOperations.java
+++ src/main/java/xyz/wagyourtail/calculator/DefaultOperations.java
@@ -9,18 +9,41 @@ import java.util.regex.Pattern;
 
 public class DefaultOperations {
 
-    public static Map<String, Function<String, String>> getFunctionOperations() {
-        Map<String, Function<String, String>> ops = new LinkedHashMap<>();
-
+    public static Map<String, Function<String[], String>> getFunctionOperations() {
+        Map<String, Function<String[], String>> ops = new LinkedHashMap<>();
+        ops.put("cos", DefaultOperations::cos);
+        ops.put("sin", DefaultOperations::sin);
+        ops.put("tan", DefaultOperations::tan);
         return ops;
     }
 
     public static Map<String, Function<String, String>> getSymbolOperations() {
         Map<String, Function<String, String>> ops = new LinkedHashMap<>();
         ops.put("\\^", DefaultOperations::power);
-        ops.put("[*/]", DefaultOperations::multiplyOrDivide);
+        ops.put("[*/%]", DefaultOperations::multiplyOrDivideOrModulo);
         ops.put("[+-]",  DefaultOperations::addOrSubtract);
         return ops;
+    }
+
+    public static Map<String, Double> getConstants() {
+        Map<String, Double> constants = new LinkedHashMap<>();
+        constants.put("pi", Math.PI);
+        return constants;
+    }
+
+    public static String cos(String[] args) {
+        if (args.length != 1) throw new RuntimeException("incorrect number of arguments for cos, expected 1");
+        return Double.toString(Math.cos(Double.parseDouble(args[0])));
+    }
+
+    public static String sin(String[] args) {
+        if (args.length != 1) throw new RuntimeException("incorrect number of arguments for sin, expected 1");
+        return Double.toString(Math.sin(Double.parseDouble(args[0])));
+    }
+
+    public static String tan(String[] args) {
+        if (args.length != 1) throw new RuntimeException("incorrect number of arguments for sin, expected 1");
+        return Double.toString(Math.tan(Double.parseDouble(args[0])));
     }
 
     public static String power(String sargs) {
@@ -32,14 +55,19 @@ public class DefaultOperations {
         return Double.toString(start);
     }
 
-    public static String multiplyOrDivide(String args) {
-        Matcher m = Pattern.compile("([*/])\\s*(" + ExtensibleCalculator.doubleMatch + ")\\s*").matcher("*" + args);
+    public static String multiplyOrDivideOrModulo(String args) {
+        Matcher m = Pattern.compile("([*/%])\\s*(" + ExtensibleCalculator.doubleMatch + ")\\s*").matcher("*" + args);
         double start = 1;
         while (m.find()) {
-            if (m.group(1).equals("*")) {
-                start *= Double.parseDouble(m.group(2));
-            } else {
-                start /= Double.parseDouble(m.group(2));
+            switch (m.group(1)) {
+                case "*":
+                    start *= Double.parseDouble(m.group(2));
+                    break;
+                case "/":
+                    start /= Double.parseDouble(m.group(2));
+                    break;
+                case "%":
+                    start %= Double.parseDouble(m.group(2));
             }
         }
         return Double.toString(start);
--- src/main/java/xyz/wagyourtail/calculator/ExtensibleCalculator.java
+++ src/main/java/xyz/wagyourtail/calculator/ExtensibleCalculator.java
@@ -3,31 +3,37 @@ package xyz.wagyourtail.calculator;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
-import java.util.Stack;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ExtensibleCalculator {
-    protected static final Map<String, Function<String, String>> functionOperations = DefaultOperations.getFunctionOperations();
+    protected static final Map<String, Function<String[], String>> functionOperations = DefaultOperations.getFunctionOperations();
     protected static final Map<String, Function<String, String>> symbolOperations = DefaultOperations.getSymbolOperations();
-    public static final String doubleMatch = "(?:-?\\d*\\.?\\d+|NaN)";
+    protected static final Map<String, Double> constants = DefaultOperations.getConstants();
+    public static final String doubleMatch = "(?:-?\\d*\\.?\\d+(?:E-?\\d+)?|NaN)";
 
 
     public static void main(String... args) {
         String input = String.join(" ", args);
-        System.out.println(calculate(input));
+        System.out.println(calculate("4 * 1 % 2 * 5"));
     }
 
 
     public static String calculate(String input) {
         //prepare input
         input = "(" + input + ")";
+        for (Map.Entry<String, Double> constant : constants.entrySet()) {
+            input = input.replaceAll("\\b" + constant.getKey() + "\\b", constant.getValue().toString());
+        }
 
         while (!input.matches(doubleMatch)) {
             String newInput = input;
+
+            //parse ready* functions
+            newInput = replaceFunction(newInput, Pattern.compile("([a-zA-Z]\\w*)\\s*\\(([^()]+)\\)"), it -> parseFunction(it[1], it[2]));
+
             //remove excess parenthesis
             newInput = replaceFunction(newInput, Pattern.compile("(\\(+)\\s*(" + doubleMatch +")\\s*(\\)+)"), it -> {
                 if (it[1].length() < it[3].length()) {
@@ -38,11 +44,10 @@ public class ExtensibleCalculator {
                 return it[2];
             });
 
-            //parse ready symbol groups
+            //parse ready* symbol groups
             newInput = replaceFunction(newInput, Pattern.compile("\\(([^()]+)\\)"), it -> parseSymbols(it[1]));
 
-            //TODO: functions
-
+            // *ready means doesn't have any nesting (grouping symbols/functions) inside that still need to be parsed.
 
             if (newInput.equals(input)) {
                 throw new RuntimeException("failed to parse \"" + input + "\" due to an unknown operation");
@@ -51,6 +56,14 @@ public class ExtensibleCalculator {
         }
 
         return input;
+    }
+
+    private static String parseFunction(String name, String combined_args) {
+        if (!functionOperations.containsKey(name)) throw new RuntimeException("function \"" + name + "\" does not exist");
+
+        //this is the only recursion and means we only go one level deep since we are parsing only "ready" functions.
+        String[] args = Arrays.stream(combined_args.split(",")).map(ExtensibleCalculator::calculate).toArray(String[]::new);
+        return functionOperations.get(name).apply(args);
     }
 
     /**
